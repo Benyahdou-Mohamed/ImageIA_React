4b1378dd32a8d2554fd7f611643435d5587a479f
benyahdou-mohamed <benyahdou.moh29@hotmail.com>
2024-02-28 18:37:33 +0100

    fix key

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import { SignIn } from '@clerk/nextjs'
 
-export const page = () => {
-  return (
-    <div>page</div>
-  )
+const SignInPage = () => {
+  return <SignIn />
 }
+
+export default SignInPage
