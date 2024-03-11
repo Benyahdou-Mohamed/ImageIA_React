4f6943808cb82f323bc63170a15ed2cb7dd6a882
benyahdou-mohamed <benyahdou.moh29@hotmail.com>
2024-03-11 18:36:02 +0100

    ajust Form for each page

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import { Header } from '../../../../../components/shared/Header'
 import { transformationTypes } from '../../../../../constants'
+import { TransformationForm } from '../../../../../components/shared/TransformationForm'
+import { auth } from '@clerk/nextjs'
+import { getUserById } from '../../../../../lib/actions/user.actions'
+import { redirect } from 'next/navigation';
 
-const AddTransformationPage = ({params:{type}}: SearchParamProps) => {
+const AddTransformationPage = async ({params:{type}}: SearchParamProps) => {
   const transformation= transformationTypes[type]
+  const {userId}= auth();
+  if(!userId){redirect('/sign-in')}
+  const user =await getUserById(userId);
+  
   return (
+    <>
     <Header title={transformation.title} subTitle={transformation.subTitle}/>
-  )
+    
+    <TransformationForm
+      action="Add"
+      userId={user._id}
+      type={transformation.type as TransformationTypeKey}
+      creditBalance={user.creditBalance}
+    />
+    </>
+    )
 }
 export default AddTransformationPage 
